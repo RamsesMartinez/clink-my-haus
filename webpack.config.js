d20bf4a5be98b8dd2c78aa4b7e64dbbf2ccc4420
Ramses Martinez <ramses.mtz96@gmail.com>
2020-01-27 20:34:58 -0600

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ module.exports = env => {
                                     enabled: false,
                                 },
                                 pngquant: {
-                                    quality: '65-90',
+                                    quality: [0.65, 0.90],
                                     speed: 4
                                 },
                                 gifsicle: {
